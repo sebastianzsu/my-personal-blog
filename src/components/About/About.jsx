80bf89099e59d7e8e2d28668bd8d2b69fc12d513
Sebastian Su <franc188@michaels.com>
2021-06-17 17:40:32 -0700

    update resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,6 +4,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Title from '../Title/Title';
 import AboutImg from '../Image/AboutImg';
 import PortfolioContext from '../../context/context';
+import pdf from './Sebastian-resume.pdf';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
@@ -49,7 +50,7 @@ const About = () => {
                       target="_blank"
                       rel="noopener noreferrer"
                       className="cta-btn cta-btn--resume"
-                      href={resume}
+                      href={pdf}
                     >
                       Resume
                     </a>
